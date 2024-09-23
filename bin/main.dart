4905677109c0d4f20d579b90b2f1b11d13229cae
helbawy <mohamed.elhelbawy.ac@gmail.com>
2024-09-23 17:17:39 +0300

    create final project folder

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_final_project/dart_final_project.dart' as dart_final_project;
+
+void main(List<String> arguments) {
+  //TODO: add your cli UI here
+}
---
+++ test/dart_final_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_final_project/dart_final_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
